7dfd73b0696b1de2ebd87866b15a88970304bc3f
Calebe de oliveira <142343433+calebeoli@users.noreply.github.com>
2025-08-27 13:29:47 -0300

    Update Tarefa.java
    Código Inicial

--- ToDoListQuarta/src/ucb/aplicacao/model/Tarefa.java
+++ ToDoListQuarta/src/ucb/aplicacao/model/Tarefa.java
@@ -1,4 +1,32 @@
 package ucb.aplicacao.model;
 
+import java.time.LocalDateTime;
+
 public class Tarefa {
+
+    private long id;
+    private String titulo;
+    private String descricao;
+    private boolean completa;
+    private LocalDateTime dataAgora;
+
+    public Tarefa(){
+        this.dataAgora = LocalDateTime.now();
+        this.completa = false;
+
+    }
+    public Tarefa(String titulo){
+        this();
+        this.titulo = titulo;
+    }
+    public Tarefa(String titulo, String descricao){
+        this(titulo);
+        this.descricao = descricao;
+    }
+    public Tarefa(Long id, String titulo, String descricao, boolean completa){
+        this(titulo, descricao);
+        this.id = id;
+        this.completa = completa;
+    }
+
 }
