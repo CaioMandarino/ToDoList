0e3cb50b9a516763e76dc02ca4e028ea6518b907
Ayslan565 <65089320+Ayslan565@users.noreply.github.com>
2025-09-03 10:12:34 -0300

    Update Getters e Setters
    Data 03/09/2025

--- ToDoListQuarta/src/ucb/aplicacao/model/Tarefa.java
+++ ToDoListQuarta/src/ucb/aplicacao/model/Tarefa.java
@@ -1,32 +1,78 @@
-package ucb.aplicacao.model;
+package ucb.aplicacao.model; //
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Tarefa {
 
-    private long id;
+    private long id; //Pode deixar a incrementação automatico
     private String titulo;
     private String descricao;
     private boolean completa;
     private LocalDateTime dataAgora;
 
+
+    //Padrão
     public Tarefa(){
         this.dataAgora = LocalDateTime.now();
         this.completa = false;
 
     }
+    //Construtor com titulo
     public Tarefa(String titulo){
         this();
         this.titulo = titulo;
     }
+    //Construtor com titulo e descrição
     public Tarefa(String titulo, String descricao){
         this(titulo);
         this.descricao = descricao;
     }
+
+    //Construtor completo
     public Tarefa(Long id, String titulo, String descricao, boolean completa){
         this(titulo, descricao);
         this.id = id;
         this.completa = completa;
     }
+    //GETS E SETS
+    public LocalDateTime getDataAgora() {
+        return dataAgora;
+    }
 
+    public void setDataAgora(LocalDateTime dataAgora) {
+        this.dataAgora = dataAgora;
+    }
+
+    public boolean isCompleta() {
+        return completa;
+    }
+
+    public void setCompleta(boolean completa) {
+        this.completa = completa;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
 }
