6a7bdfc4e6e83affdec0a6ec3c0cb97229116076
Caio <caiomand04@gmail.com>
2025-09-03 10:40:41 -0300

    Criação do create em tarefaservice

--- ToDoListQuarta/src/ucb/aplicacao/cli/Main.java
+++ ToDoListQuarta/src/ucb/aplicacao/cli/Main.java
@@ -1,12 +1,10 @@
 package ucb.aplicacao.cli;
 
 
+import ucb.aplicacao.service.TarefaService;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
-        }
+        TarefaService service = new TarefaService();
     }
 }
--- ToDoListQuarta/src/ucb/aplicacao/service/TarefaService.java
+++ ToDoListQuarta/src/ucb/aplicacao/service/TarefaService.java
@@ -1,4 +1,19 @@
 package ucb.aplicacao.service;
 
+import ucb.aplicacao.model.Tarefa;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class TarefaService {
+    private List<Tarefa> tarefas = new ArrayList<Tarefa>();
+
+    public void criarTarefa(String nome, String descricao) {
+        Tarefa tarefa = new Tarefa(nome, descricao);
+        tarefas.add(tarefa);
+    }
+
+    public List<Tarefa> getTarefas() {
+        return tarefas;
+    }
 }
