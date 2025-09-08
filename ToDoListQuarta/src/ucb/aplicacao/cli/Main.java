8ee897cd49f14c9dead7cad874c9691b1573edaa
Ayslan565 <ayslan565@gmail.com>
2025-09-07 23:04:20 -0300

    Update 1.0 Correção de bugs do titulo e descrição Atualização para o Caio

--- ToDoListQuarta/src/ucb/aplicacao/cli/Main.java
+++ ToDoListQuarta/src/ucb/aplicacao/cli/Main.java
@@ -39,7 +39,6 @@ public class Main {
 
 
                 Tarefa nova = service.criarTarefa(titulo,descricao,completa);
-
                 nova.getDataAgora();
                 System.out.println("==================================================");
                 System.out.println("                    Resultados");
@@ -52,10 +51,18 @@ public class Main {
             }
 
             case 2 -> {
+                List<Tarefa> tarefas = service.listarTarefas();
+                if (tarefas.isEmpty()){
+                    System.out.println("A lista esta vazia");
+                }
+                else {
+
+                }
 
             }
             case 3 -> {
                 System.out.println("========= Atualizar Tarefa =========");
+
             }
             case 4 -> {
                 System.out.println("========= Remover Tarefa =========");
