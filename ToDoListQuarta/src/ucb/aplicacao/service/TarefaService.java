a939a66903bb1b72badf26aa2f80d0f1dcc4f03e
Ayslan565 <65089320+Ayslan565@users.noreply.github.com>
2025-09-08 17:57:42 -0300

    Revert "Update"
    This reverts commit 8263efb9eb99ca250f16259654ae26f78c46c54f.

--- ToDoListQuarta/src/ucb/aplicacao/service/TarefaService.java
+++ ToDoListQuarta/src/ucb/aplicacao/service/TarefaService.java
@@ -8,7 +8,6 @@ import java.util.List;
 public class TarefaService {
     private List<Tarefa> tarefas = new ArrayList<Tarefa>();
 
-<<<<<<< Updated upstream
     public Tarefa criarTarefa(String nome, String descricao) {
         Tarefa tarefa = new Tarefa(nome, descricao);
         tarefas.add(tarefa);
@@ -27,34 +26,3 @@ public class TarefaService {
         return tarefas;
     }
 }
-=======
-        private List<Tarefa> tarefas = new ArrayList<Tarefa>();
-        private long id = 0;
-
-        public Tarefa criarTarefa (String titulo, String descricao,boolean completa) {
-
-            Tarefa tarefa = new Tarefa(id++ ,titulo, descricao, completa);
-                 tarefas.add(tarefa);
-                 tarefas.add(tarefa);
-
-
-            return tarefa;
-
-        }
-
-        public List<Tarefa> listarTarefas() {
-           // for(int i =0; i< tarefas.size(); i+=1){
-                return listarTarefas();
-
-                }
-
-        public Tarefa consultarTarefa(long id ){
-                for (Tarefa tarefa  : tarefas ){
-                    if (tarefa.getId().equals(id)){
-                        return tarefa;
-                    }
-            }
-                return null;
-            }
-        }
->>>>>>> Stashed changes
